2048338b4bf89076fb1689a54dc1bd2ff4ae872b
RianDoang <muhamadrynsyy270521@gmail.com>
2023-12-26 00:36:01 +0700

    Perubahan UI mobile

--- src/script.js
+++ src/script.js
@@ -1,3 +1,25 @@
+function redirectHTML() {
+  window.location.href = "tes.html";
+}
+
+// Hamburger menu
+const hamburger = document.querySelector(".hamburger-menu");
+const navMenu = document.querySelector("nav ul");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("show");
+});
+
+// Klik diluar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.remove("show");
+  }
+});
+
+// Catch Data User
 const scriptURL =
   "https://script.google.com/macros/s/AKfycby9srbt7vECeWaWPwOQ-G43vJh4udoQlWyUBLjQ-KMMBvcVaVqSHFzWKiF-FSqa3n4bOA/exec";
 const form = document.forms["submit-to-google-sheet"];
