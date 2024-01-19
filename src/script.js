78491deb4e447fafef4112dcde1e72264d25fd26
RianDoang <muhamadrynsyy270521@gmail.com>
2024-01-19 17:26:52 +0700

    menambah user login page

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 function redirectHTML() {
-  window.location.href = "tes.html";
+  window.location.href = "login-user/unlock.html";
 }
 
 // Hamburger menu
@@ -17,16 +17,4 @@ window.addEventListener("click", function (e) {
     hamburger.classList.remove("hamburger-active");
     navMenu.classList.remove("show");
   }
-});
-
-// Catch Data User
-const scriptURL =
-  "https://script.google.com/macros/s/AKfycby9srbt7vECeWaWPwOQ-G43vJh4udoQlWyUBLjQ-KMMBvcVaVqSHFzWKiF-FSqa3n4bOA/exec";
-const form = document.forms["submit-to-google-sheet"];
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  fetch(scriptURL, { method: "POST", body: new FormData(form) })
-    .then((response) => console.log("Success!", response))
-    .catch((error) => console.error("Error!", error.message));
 });
